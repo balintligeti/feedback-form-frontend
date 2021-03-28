e6313571ab5b4d4591f14535e313f54154c964a4
Balint Ligeti <balint.ligeti@hotmail.com>
2021-03-28 19:50:50 +0200

    textbox width again, mobile view

--- src/app/service/api.service.ts
+++ src/app/service/api.service.ts
@@ -17,8 +17,12 @@ export class ApiService {
   }
  */
 
+  
+
   addFeedback(userFeedback: UserFeedback): Observable<any> {
-    const headers = { 'content-type': 'application/json', Authorization: `Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoiMSJ9.VzqrIt7rU5JEQzVsgk-hxGr56VphfQF9h5KnpOhyYvk`}  
+    const headers = { 'content-type': 'application/json',
+     'Authorization': `Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoiMSJ9.VzqrIt7rU5JEQzVsgk-hxGr56VphfQF9h5KnpOhyYvk`,
+    }  
     const body=JSON.stringify(userFeedback);
     console.log(body, headers)
     return this.http.post(this.baseURL + 'feedbacks', body,{'headers':headers})
